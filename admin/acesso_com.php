e029283f0d7bcabb16d2ea19f72aca55523c6e9b
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-09-18 10:04:57 -0300

    Ambiente pronto para seguir com o acesso de admin!

---
+++ admin/acesso_com.php
@@ -0,0 +1,23 @@
+<?php 
+
+// 1 - Definir nome da sessão 
+
+
+
+// 2 - Segurança: verificar se a sessão é valida
+
+
+
+
+// 3 - Verifica se o nome da sessão corresponde a sessão atual 
+
+
+
+// 4 - Segurança Extra: valida o agente (usuário) e o IP
+
+
+// 5 - Se IP ou navegador mudaram, invalida a sessão!
+
+
+
+?>
