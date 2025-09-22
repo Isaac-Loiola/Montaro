ea99d74e5fc8e9ba8dfff65b105238466cd3012a
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-09-22 11:37:06 -0300

    Classe Tipo criada, e função inserir

---
+++ php/class/tipo.php
@@ -0,0 +1,27 @@
+<?php 
+include "db.php";
+
+class Tipo{
+    // Atributos!
+    private $id;
+    private $sigla;
+    private $rotulo;
+
+    private $pdo;
+
+    function __construct()
+    {
+        $this->pdo = getConnection();
+    }
+
+    // Função para inserir um tipo ao banco de dados.
+    function inserir(string $sigla, string $rotulo) : bool {
+        $sql = "insert into tipos (sigla, rotulo) values (:sigla, :rotulo)";
+        $cmd = $this->pdo->prepare($sql);
+        return $cmd->execute([
+            ':sigla' => $sigla,
+            ':rotulo' => $rotulo
+        ]);
+    }
+}
+?>
