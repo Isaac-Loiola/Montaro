d52b170364fc25c1b4fffa2fc65336f37e6232a1
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-09-23 08:17:58 -0300

    Atributos e a função inserir adicionado a classe Nivel.

--- php/class/nivel.php
+++ php/class/nivel.php
@@ -1,5 +1,27 @@
 <?php 
 
+include_once "db.php";
 
+class Nivel{
+    private $id;
+    private $sigla;
+    private $nome;
+
+    private $pdo;
+
+    function __construct()
+    {
+        $this->pdo = getConnection();
+    }
+
+    function inserir(string $sigla, string $nome):bool{
+        $sql = "insert into niveis(sigla, nome) values (:sigla, :nome)";
+        $cmd = $this->pdo->prepare($sql);
+        return $cmd->execute([
+            ':sigla' => $sigla,
+            ':nome' => $nome
+        ]);
+    }
+}
 
 ?>
