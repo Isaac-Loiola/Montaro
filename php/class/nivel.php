c97a31b4e2e92c29f50f29f5b01dbf2c5820edfb
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-09-23 08:31:00 -0300

    Classe nivel finalizada!

--- php/class/nivel.php
+++ php/class/nivel.php
@@ -18,20 +18,33 @@ class Nivel{
     function inserir(string $sigla, string $nome):bool{
         $sql = "insert into niveis(sigla, nome) values (:sigla, :nome)";
         $cmd = $this->pdo->prepare($sql);
-        return $cmd->execute([
-            ':sigla' => $sigla,
-            ':nome' => $nome
-        ]);
+        $cmd->bindValue(':sigla', $sigla);
+        $cmd->bindValue(':nome', $nome);
+        return $cmd->execute();
     }
 
     function obterPorId(string $idNivel): array{
         $sql = "select * from niveis where id = :id";
         $cmd = $this->pdo->prepare($sql);
-        $cmd->execute([
-            ':id' => $idNivel
-        ]);
+        $cmd->bindValue(':id', $idNivel);
+        $cmd->execute();
 
         return $cmd->fetch();
+    }
+
+    function obterLista() : array{
+        $sql = "select * from niveis";
+        $cmd = $this->pdo->query($sql);
+
+        return $cmd->fetchAll();
+    }
+
+    function atualizar(){
+        $sql = "update niveis set sigla = :sigla, nome = :nome";
+        $cmd = $this->pdo->prepare($sql);
+        $cmd->bindValue(':sigla', $this->sigla);
+        $cmd->bindValue(':nome', $this->nome);
+        $cmd->execute();
     }
 }
 
