ffa9100fe51d471f8c373c23570a702db66757c6
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-10-13 11:48:27 -0300

    Menu_cliente adicionado!

--- cliente/index.php
+++ cliente/index.php
@@ -1,8 +1,6 @@
 <?php 
-    // include_once "../admin/acesso_com.php";
+    include "../admin/acesso_com.php";
 
-    session_name("montaro");
-    session_start();
 ?>
 <!DOCTYPE html>
 <html lang="PT-BR">
@@ -10,8 +8,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Montaro - Área do Cliente</title>
+
+    <!--  icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    
+    <!--  boostrap 5.3 local -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+    <!-- bootstrap javaScript -->
+    <script src="../js/bootstrap.min.js" defer></script>
+    <script src="..js/bootstrap.bundle.min.js" defer></script>
+
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    
+    <header>
+        <?php 
+            // include "menu_cliente.php";
+        ?>
+    </header>
 </body>
 </html>
---
+++ cliente/menu_cliente.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-expand-xl fixed-top ">
+           <div class="container-fluid">
+               <a href="../php/index.php" class="navbar-brand">
+                   <img src="../images/logo/montaro-logo-white.png" alt>
+               </a>
+               <button class="navbar-toggler" 
+               type="button" 
+               data-bs-target="#menuPublico" 
+               data-bs-toggle="collapse" 
+               aria-controls="menuPublico"
+               aria-expanded="false"
+               aria-label="Toggle navigation"
+               >
+                   <span class="bi bi-three-dots"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="menuPublico">
+                   <ul class="navbar-nav ms-auto mb-2 mb-md-0 gap-2" >
+                       <li class="nav-item">
+                           <a href="index.html" class="nav-link active" aria-current="page">
+                               <i class="bi bi-house-door-fill text-white"></i>
+                           </a>
+                       </li>
+                        <li class="nav-item ">
+                            <a href="produtos_lista.php" class="nav-link text-white">
+                                Produtos
+                            </a>
+                        </li>
+                          <li class="nav-item">
+                              <a href="tipos_lista.php" class="nav-link text-white">
+                                  Tipos
+                              </a>
+                          </li>
+                          <li class="nav-item">
+                              <a href="usuarios_lista.php" class="nav-link text-white">
+                                  Usuarios
+                              </a>
+                          </li>
+                          <li class="nav-item"> 
+                              <a href="logout.php" class="nav-link text-white">
+                                  <i class="bi bi-arrow-right"></i>
+                              </a>
+                          </li>
+                   </ul>
+               </div>
+           </div>
+        </nav>
