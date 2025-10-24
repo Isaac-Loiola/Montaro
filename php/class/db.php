00ae4e095e75d76b0b7521d22a3a827a361db687
isaac-loiola <isaackopi@icloud.com>
2025-10-24 20:49:01 -0300

    Usuario de banco de dados refatorado!

--- php/class/db.php
+++ php/class/db.php
@@ -16,7 +16,7 @@
                 // senha notebook
                 "mysql:host=127.0.0.1;dbname=tdszuphpdb01", 
                 "root", 
-                ""
+                "adsz..XcW21034",
                 [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
             );
