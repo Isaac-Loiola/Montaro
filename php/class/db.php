a48a12e08ca8df8e46c68d360235e3987b28c3e8
isaac-loiola <isaackopi@icloud.com>
2025-10-24 20:10:57 -0300

    Alterando banco de dados para notebook

--- php/class/db.php
+++ php/class/db.php
@@ -3,12 +3,20 @@
         static $pdo;
         if( $pdo === null){
             $pdo = new PDO(
+                // senha desktop
                 // "mysql:host=127.0.0.1;dbname=tdszuphpdb01", 
                 // "root", 
+
+                // senha senac
                 // "adsz..XcW21034",
-                "mysql:host=10.91.47.77;dbname=tdszuphpdb01", 
+                // "mysql:host=10.91.47.77;dbname=tdszuphpdb01", 
+                // "root", 
+                // "123",
+
+                // senha notebook
+                "mysql:host=127.0.0.1;dbname=tdszuphpdb01", 
                 "root", 
-                "123",
+                ""
                 [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
             );
